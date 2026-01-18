06d502d79c099b00743c743ce12a3507f7568fd2
Martin Bibb <martinbibb@gmail.com>
2026-01-18 10:09:22 +0000

    fix(shared): ESM-safe exports (explicit .js, no directory barrels)

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,13 +1,17 @@
-export * from './types';
-export * from './types/heat-loss-survey.types';
-export * from './types/gc-catalog.types';
-export * from './types/mains-test.types';
-export * from './surveyHelperSlots';
-export * from './moduleRegistry';
-export * from './core';
-export * from './rocky';
-export * from './sarah';
-export * from './utils/heat-loss-physics';
-export * from './utils/atlas-heat-loss';
-export * from './job-graph';
-export * from './heating-design';
+export * from './types.js';
+export * from './types/heat-loss-survey.types.js';
+export * from './types/gc-catalog.types.js';
+export * from './types/mains-test.types.js';
+
+export * from './surveyHelperSlots.js';
+export * from './moduleRegistry.js';
+
+export * from './core/index.js';
+export * from './rocky/index.js';
+export * from './sarah/index.js';
+
+export * from './utils/heat-loss-physics.js';
+export * from './utils/atlas-heat-loss.js';
+
+export * from './job-graph/index.js';
+export * from './heating-design/index.js';
